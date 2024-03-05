25f9b713b06efac25900108edc12d4b53d2780f3
mohamadjisri <mohammadjisri@gmail.com>
2024-03-05 09:50:23 +0100

    Files sorting

---
+++ src/components/1-header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./header.css";
+
+export default function Header() {
+  return (
+    <header>
+      <nav></nav>
+
+      <button></button>
+    </header>
+  );
+}
